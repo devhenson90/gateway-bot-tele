b4c9dc2c9d0f23093cf70e77a20c479faaaf4f0f
neywannabillion <chatchai.p@nexunify.com>
2025-05-28 12:30:31 +0700

    #feat: add chat support to whitelist

--- src/bot-telegram/bll/chat-ids.ts
+++ src/bot-telegram/bll/chat-ids.ts
@@ -1,3 +1,3 @@
-export const chatIds: number[] = [5781571528,-4541864058,-4646207033,7352289443];
+export const chatIds: number[] = [5781571528,-4541864058,-4646207033,7352289443,-4638853566];
 
 module.exports = { chatIds };
